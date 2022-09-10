092b3718530cf5b45d50edc786729cbefda3af30
vishalrawat64 <93420999+vishalrawat64@users.noreply.github.com>
2022-09-10 12:55:29 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 //const api_url = "<heroku_app_url>"
-const api_url = "http://localhost:8080/user"
+const api_url = "https://esdmevnproject.herokuapp.com/user"
 
 function loadData(records = []) {
 	var table_data = "";
